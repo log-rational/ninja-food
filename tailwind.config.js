665686435795e2a39a1091923c9c7cf2010ce20b
Umesh Rai <Umesh.Rai508@outlook.com>
2021-08-29 18:25:49 +0100

    complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,12 @@ module.exports = {
       colors: {
         primary: "#ff6363",
         secondary: {
-          100: '#e2e2d5',
-          200: '#888883'
-        }
+          100: "#e2e2d5",
+          200: "#888883",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito"],
       },
     },
   },
